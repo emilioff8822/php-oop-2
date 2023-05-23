b975822f9acc82048af6717ed96d325a1b8bd142
emilioff8822 <emilioff@hotmail.it>
2023-05-23 14:16:45 +0200

    commit-array-prodotti

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php
+
 include_once __DIR__ .'/models/Cane.php';
 include_once __DIR__ .'/models/Gatto.php';
 include_once __DIR__ .'/models/Prodotto.php';
 
-
-
+$arrayProdotti=[
+    new Prodotto('Cibo per cani', 'Pedigree', 10.99, 'Cibo', new Cane('Cane', 'Labrador'), 'https://picsum.photos/id/237/200/300'),
+    new Prodotto('Cibo per gatti', 'Whiskas', 8.99, 'Cibo', new Gatto('Gatto', 'Siamese'), 'https://picsum.photos/id/1025/200/300'),
+    new Prodotto('Cuccia per cani', 'Ferplast', 45.00, 'Cuccia', new Cane('Cane', 'Bulldog'), 'https://picsum.photos/id/433/200/300'),
+];
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
